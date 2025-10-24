ec92c7cdc37f834f84a87284478b58f3c88ac0a4
Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>
2025-10-24 11:24:24 -0400

    Update setup.tsx
    Signed-off-by: Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>

--- src/setup.tsx
+++ src/setup.tsx
@@ -3,7 +3,7 @@ import { PopupMessages } from "@/bits/popup-messages";
 import { Toaster as QuickMessages } from "@/components/user-interface/sonner";
 import { TooltipProvider } from "@/components/user-interface/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
 import ModelTrainer from "./views/ModelTrainer";
 import StatsLab from "./views/StatsLab";
@@ -20,14 +20,14 @@ const AppSetup = () => (
       <QuickMessages />
       
       {/* main routing */}
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/train-model" element={<ModelTrainer />} />
           <Route path="/analyze-data" element={<StatsLab />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
