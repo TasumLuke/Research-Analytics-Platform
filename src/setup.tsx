c2f85dec4ae1b55338292df87c9848f123d1e7c6
Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>
2025-10-30 01:08:23 -0400

    Update setup.tsx
    Signed-off-by: Luke Rimmo Loyi Lego <114318995+TasumLuke@users.noreply.github.com>

--- src/setup.tsx
+++ src/setup.tsx
@@ -3,13 +3,13 @@ import { PopupMessages } from "@/bits/popup-messages";
 import { Toaster as QuickMessages } from "@/components/user-interface/sonner";
 import { TooltipProvider } from "@/components/user-interface/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
 import ModelTrainer from "./views/ModelTrainer";
 import StatsLab from "./views/StatsLab";
 import NotFound from "./views/NotFound";
 
-// this basically have react query for fetching stuff
+// gotta have react query for fetching stuff
 const queryClient = new QueryClient();
 
 const AppSetup = () => (
@@ -20,14 +20,14 @@ const AppSetup = () => (
       <QuickMessages />
       
       {/* pages */}
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/train-model" element={<ModelTrainer />} />
           <Route path="/analyze-data" element={<StatsLab />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
